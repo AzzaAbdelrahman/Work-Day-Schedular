0af830c1dcce0af64b9b6a6ac3eea32e39311b9f
AzzaAbdelrahman <148109380+AzzaAbdelrahman@users.noreply.github.com>
2023-12-12 20:13:20 +0000

    complete

--- assets/index.js
+++ assets/index.js
@@ -1,38 +1,41 @@
-const localStorageData = [{
-    date: '08.12-2023',
-    data: {
-        9: 'text at 9AM',
-        10: 'text at 10AM',
-        11: 'text at 11AM',
-        12: 'text at 12PM',
-        1: 'text at 1PM',
-        2: 'text at 2PM',
-        3: 'text at 3PM',
-        4: 'text at 4PM',
-        5: 'text at 5PM',
-       
-    },
-}]
 
-const today = dayjs().format('DD.MM.YYY');
-
-const todayDataIndex = localStorageData.findIndex(function(element){
-    return element.date === today;
-
-});
-
-if (todayDataIndex < 0){
-    const todayData = localStorageData[todayDataIndex];
-    document.getElementById('time-block-9').value = data9AM;
-    document.getElementById('time-block-10').value = data10AM;
-    document.getElementById('time-block-11').value = data11AM;
-    document.getElementById('time-block-12').value = data12PM;
-    document.getElementById('time-block-1').value = data1PM;
-    document.getElementById('time-block-2').value = data2PM;
-    document.getElementById('time-block-3').value = data3PM;
-    document.getElementById('time-block-4').value = data4PM;
-    document.getElementById('time-block-5').value = data5PM;
+$(function () {
+    var saveBtn = $(".saveBtn");
+    var tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+    var today = dayjs();
+    var currentDay = $("#currentDay");
+    var currentHour = parseInt(today.format("H"));
     
+    // Event listener for click on saveBtn
+    saveBtn.on("click", storeTasks);
+    
+    for (const key in tasks) {
+      var timeBlock = $("#" + key).children()[1];
+      timeBlock.value = tasks[key];
+    }
+    // Create function to store tasks
+    function storeTasks(event) {
+      var storeTask = {
+        task: $(this).siblings()[1].value,
+        time: $(this).parent().attr("id"),
+      };
+  
+      tasks[storeTask.time] = storeTask.task;
+      // Set items in local storage
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 
-};
-console.log= todayDataIndex;
+    currentDay.text(today.format("dddd D, MMMM ,YYYY h:mm:ss A"));
+  
+    for (var i = 9; i < 18; i++) {
+      var relativeTime = $("#hour-" + i);
+      if (i < currentHour) {
+        relativeTime.addClass("past");
+      } else if (i === currentHour) {
+        relativeTime.addClass("present");
+      } else {
+        relativeTime.addClass("future");
+      }
+    }
+    
+  });
